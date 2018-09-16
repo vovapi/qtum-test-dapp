4832d6a205537b47770818aed28f59ca1ebed34e
Vladimir Evgrafov <evgrafov.vladimir@gmail.com>
2018-09-17 01:15:46 +0300

    pre-release

--- src/index.js
+++ src/index.js
@@ -2,23 +2,30 @@ import $ from 'jquery';
 window.$ = $;
 window.jQuery;
 import Qweb3 from 'qweb3';
+import 'bootstrap';
+import './scss/index.scss';
 
 var client;
+
+function updateAccount(account) {
+    console.log('account', account);
+    if (account && account.balance) {
+        document.getElementById("main").classList.remove('d-none')
+        document.getElementById("address").textContent = account.address;
+        document.getElementById("balance").textContent = account.balance.toString() + " QTUM";
+    }
+}
+
 $(function() {
     function qryptoAcctChanged(event) {
+    console.log(event);
     if (event.data.message && event.data.message.type == "ACCOUNT_CHANGED") {
         if (event.data.message.payload.error){
             console.log("error:", event.data.message.payload.error);
             return;
         }
         console.log("account:", event.data.message.payload.account)
-        if (event.data.message.payload.account) {
-            var account = event.data.message.payload.account;
-            document.getElementById("address").innerText = account.address;
-            document.getElementById("name").innerText = account.name;
-            document.getElementById("network").innerText = account.network;
-            document.getElementById("balance").innerText = account.balance;
-        }
+        updateAccount(event.data.message.payload.account);
         }
     }
     window.addEventListener('message', qryptoAcctChanged, false);
@@ -29,6 +36,8 @@ $(function() {
         client = new Qweb3.Qweb3(window.qrypto.rpcProvider);
         console.log(client);
         window.Qweb3Client = client;
-        
+        if (window.qrypto.account) {
+            updateAccount(window.qrypto.account);
+        };
     })
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,42 @@
 module.exports = {
-    entry: [
-      './src/index.js'
-    ],
-    output: {
-      path: __dirname,
-      publicPath: '/',
-      filename: 'bundle.js'
-    }
-}
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
